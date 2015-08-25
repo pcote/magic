e9e7bf22555d41f81053949b0128ceb5f9893afc
pcote <cotejrp@gmail.com>
2015-08-25 14:02:33 -0400

    Removal of obsolete services that are now taken care of by 'getinfo'

--- service.py
+++ service.py
@@ -8,7 +8,6 @@ from configparser import ConfigParser
 import tabledefs
 
 
-
 app = Flask(__name__)
 
 parser = ConfigParser()
@@ -46,28 +45,6 @@ def get_card(card_id):
     return jsonify(return_result)
 
 
-@app.route("/strength")
-def search_strength():
-
-    power = request.args.get("power")
-    toughness = request.args.get("toughness")
-
-    clause_list = list()
-    clause_list.append(strength_table.c.power == power)
-
-    query = db.select([card_table.c.name, card_table.c.type, card_table.c.rarity, card_table.c.artist, card_set_table.c.name])
-    query = query.select_from(strength_table.join(card_table.join(card_set_table)))
-    if power:
-        query = query.where(strength_table.c.power == power)
-    if toughness:
-        query = query.where(strength_table.c.toughness == toughness)
-
-    data = list(__runquery(query).fetchall())
-    data_set = [dict(name=name, type=type, rarity=rarity, artist=artist, set_name=set_name) for name, type, rarity, artist, set_name in data]
-    return jsonify({"results":data_set})
-
-
-
 @app.route("/setinfo/<s_code>")
 def get_set_info(s_code):
     query = db.select([card_set_table])
@@ -79,42 +56,6 @@ def get_set_info(s_code):
 
     return jsonify({"results":set_info})
 
-
-@app.route("/loyalty/<loyalty_num>")
-def get_loyalty_info(loyalty_num):
-    query = db.select([card_table.c.name, card_table.c.type, card_table.c.rarity, card_table.c.artist, card_set_table.c.name])
-    query = query.select_from(loyalty_table.join(card_table.join(card_set_table)))
-    query = query.where(loyalty_table.c.loyalty == loyalty_num)
-    res = __runquery(query)
-    res = res.fetchall()
-    data_set = [dict(name=name, type=type, rarity=rarity, artist=artist, set_name=set_name) for name, type, rarity, artist, set_name in res]
-    return jsonify({"results":data_set})
-
-
-@app.route("/color")
-def get_color_info():
-    color_arg = request.args.get("color")
-    color_arg = color_arg.title()
-    query = db.select([card_table.c.name, card_table.c.type, card_table.c.rarity, card_table.c.artist, card_set_table.c.name])
-    query = query.select_from(color_table.join(card_table.join(card_set_table)))
-    query = query.where(color_table.c.color_name == color_arg)
-    res = __runquery(query)
-    res = res.fetchall()
-    data_set = [dict(name=name, type=type, rarity=rarity, artist=artist, set_name=set_name) for name, type, rarity, artist, set_name in res]
-    return jsonify({"results":data_set})
-
-
-@app.route("/text")
-def get_text_info():
-    text_arg = request.args.get("text")
-    query = db.select([card_table.c.name, card_table.c.type, card_table.c.rarity, card_table.c.artist, card_set_table.c.name])
-    query = query.select_from(text_table.join(card_table.join(card_set_table)))
-    query = query.where(text_table.c.text.like("%{}%".format(text_arg)))
-    print(query)
-    res = __runquery(query)
-    res = res.fetchall()
-    data_set = [dict(name=name, type=type, rarity=rarity, artist=artist, set_name=set_name) for name, type, rarity, artist, set_name in res]
-    return jsonify({"results":data_set})
 
 @app.route("/getinfo")
 def get_info():
